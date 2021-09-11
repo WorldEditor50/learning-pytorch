9425af838157b9606579c9b9d77e592bc3e78734
WorldEditor50 <1853288789@qq.com>
2021-09-11 20:30:12 +0800

    FooNN

---
+++ FooNN/model.py
@@ -0,0 +1,22 @@
+from torch.nn.modules.conv import Conv2d
+from torch.nn.modules.pooling import MaxPool2d
+from torch import nn
+
+class FooNN(nn.Module):
+    def __init__(self):
+        super(FooNN, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+    
+    def forward(self, x):
+        x = self.model(x)
+        return x
